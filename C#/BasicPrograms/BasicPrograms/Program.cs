91c607a7aa6fb2ada376c178777cbb29ab3e4be2
PiyushPattanayak <piyush.pattanayak@gmail.com>
2017-07-22 03:33:37 -0700

    Adding projects for a basic programs library and runner

---
+++ C#/BasicPrograms/BasicPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using BasicProgramsLibrary;
+
+namespace BasicPrograms
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.Run();
+        }
+    }
+}
---
+++ C#/BasicPrograms/BasicProgramsLibrary/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BasicProgramsLibrary
+{
+    public class HelloWorld
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
